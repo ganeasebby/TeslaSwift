85feb3f482710c21fcd5509b023ba220b189312d
Sebastian Ganea <ganeasebby@yahoo.com>
2024-03-03 00:57:50 +0200

    ...

--- Sources/Extensions/StreamingCombine/TeslaSwift+StreamingCombine.swift
+++ Sources/Extensions/StreamingCombine/TeslaSwift+StreamingCombine.swift
@@ -10,51 +10,51 @@
 import Combine
 import TeslaSwift
 
-extension TeslaStreaming  {
-    public func streamPublisher(vehicle: Vehicle) -> TeslaStreamingPublisher {
-        return TeslaStreamingPublisher(vehicle: vehicle, stream: self)
-    }
-
-    public struct TeslaStreamingPublisher: Publisher, Cancellable {
-        public typealias Output = TeslaStreamingEvent
-        public typealias Failure = Error
-
-        let vehicle: Vehicle
-        let stream: TeslaStreaming
-
-        init(vehicle: Vehicle, stream: TeslaStreaming) {
-            self.vehicle = vehicle
-            self.stream = stream
-        }
-
-        public func receive<S>(subscriber: S) where S: Subscriber, TeslaStreamingPublisher.Failure == S.Failure, TeslaStreamingPublisher.Output == S.Input {
-
-            Task {
-                do {
-                    for try await streamEvent in try await stream.openStream(vehicle: vehicle) {
-                        switch streamEvent {
-                            case .open:
-                                _ = subscriber.receive(TeslaStreamingEvent.open)
-                            case .event(let event):
-                                _ = subscriber.receive(TeslaStreamingEvent.event(event))
-                            case .error(let error):
-                                _ = subscriber.receive(TeslaStreamingEvent.error(error))
-                            case .disconnected:
-                                _ = subscriber.receive(TeslaStreamingEvent.disconnected)
-                                subscriber.receive(completion: Subscribers.Completion.finished)
-                        }
-                    }
-                } catch let error {
-                    _ = subscriber.receive(TeslaStreamingEvent.error(error))
-                    subscriber.receive(completion: Subscribers.Completion.finished)
-                }
-            }
-        }
-
-        public func cancel() {
-            stream.closeStream()
-        }
-    }
-}
+//extension TeslaStreaming  {
+//    public func streamPublisher(vehicle: Vehicle) -> TeslaStreamingPublisher {
+//        return TeslaStreamingPublisher(vehicle: vehicle, stream: self)
+//    }
+//
+//    public struct TeslaStreamingPublisher: Publisher, Cancellable {
+//        public typealias Output = TeslaStreamingEvent
+//        public typealias Failure = Error
+//
+//        let vehicle: Vehicle
+//        let stream: TeslaStreaming
+//
+//        init(vehicle: Vehicle, stream: TeslaStreaming) {
+//            self.vehicle = vehicle
+//            self.stream = stream
+//        }
+//
+//        public func receive<S>(subscriber: S) where S: Subscriber, TeslaStreamingPublisher.Failure == S.Failure, TeslaStreamingPublisher.Output == S.Input {
+//
+//            Task {
+//                do {
+//                    for try await streamEvent in try await stream.openStream(vehicle: vehicle) {
+//                        switch streamEvent {
+//                            case .open:
+//                                _ = subscriber.receive(TeslaStreamingEvent.open)
+//                            case .event(let event):
+//                                _ = subscriber.receive(TeslaStreamingEvent.event(event))
+//                            case .error(let error):
+//                                _ = subscriber.receive(TeslaStreamingEvent.error(error))
+//                            case .disconnected:
+//                                _ = subscriber.receive(TeslaStreamingEvent.disconnected)
+//                                subscriber.receive(completion: Subscribers.Completion.finished)
+//                        }
+//                    }
+//                } catch let error {
+//                    _ = subscriber.receive(TeslaStreamingEvent.error(error))
+//                    subscriber.receive(completion: Subscribers.Completion.finished)
+//                }
+//            }
+//        }
+//
+//        public func cancel() {
+//            stream.closeStream()
+//        }
+//    }
+//}
 
 #endif
